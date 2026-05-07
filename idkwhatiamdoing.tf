966d52a7c8cc06970e2b8cb12813ac8d4e4991a6
Zachary Lim <zachary_limjunsheng@hotmail.com>
2026-05-07 20:56:11 +0800

    Format required ver

--- idkwhatiamdoing.tf
+++ idkwhatiamdoing.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket = "sctp-ce12-tfstate-bucket"   # Change this
     key    = "ce12-mod3_2-zaclim.tfstate" # Change this
@@ -13,3 +22,4 @@ terraform {
 resource "aws_s3_bucket" "s3_tf" {
   bucket_prefix = "zac_lim" # Set your bucket name here
 }
+
