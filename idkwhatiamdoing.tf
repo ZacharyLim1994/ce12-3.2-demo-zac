b60ed87ac61957c4d0554611c6210cda204fb857
Zachary Lim <zachary_limjunsheng@hotmail.com>
2026-05-07 21:21:33 +0800

    Fix Checkov skip comments

--- idkwhatiamdoing.tf
+++ idkwhatiamdoing.tf
@@ -20,6 +20,14 @@ terraform {
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix = "zac_lim" # Set your bucket name here
+  #checkov:skip=CKV_AWS_18: Demo bucket does not require access logging
+  #checkov:skip=CKV_AWS_21: Demo bucket does not require versioning
+  #checkov:skip=CKV_AWS_144: Demo bucket does not require cross-region replication
+  #checkov:skip=CKV_AWS_145: Demo bucket does not require KMS encryption
+  #checkov:skip=CKV2_AWS_61: Demo bucket does not require lifecycle configuration
+  #checkov:skip=CKV2_AWS_62: Demo bucket does not require event notification
+  #checkov:skip=CKV2_AWS_6: Demo bucket does not require public access block configuration for this lab
+
+  bucket_prefix = "zac-lim" # Set your bucket name here
 }
 
